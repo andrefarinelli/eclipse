b1dfa89a145eff86455015c94d4bd851cb8cdf1e
andrefarinelli <70445801+andrefarinelli@users.noreply.github.com>
2020-10-23 00:33:09 -0300

    Add files via upload
    First Blue Food commit

---
+++ BlueFood/src/main/java/com/bluefood/BlueFoodApplication.java
@@ -0,0 +1,13 @@
+package com.bluefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueFoodApplication.class, args);
+	}
+
+}
---
+++ BlueFood/src/main/java/com/bluefood/domain/cliente/Cliente.java
@@ -0,0 +1,19 @@
+package com.bluefood.domain.cliente;
+
+import javax.persistence.Entity;
+import com.bluefood.domain.usuario.Usuario;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@SuppressWarnings("serial")
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = true)
+@Entity
+public class Cliente extends Usuario {
+
+	private String cpf;
+	
+	private String cep;
+}
